4ade703e42d2d3f847a657e9fdf5d6f50c773a98
Keaton Okkonen <keaton.okkonen@gmail.com>
2015-08-27 23:43:52 +1000

    upload

--- player.c
+++ player.c
@@ -1,6 +1,6 @@
 /***********************************************************************
 * COSC1076 - Advanced Programming Techniques
-* Semester 2 2015 Assignment #1 
+* Semester 2 2015 Assignment #1
 * Full Name        : EDIT HERE
 * Student Number   : EDIT HERE
 * Course Code      : EDIT HERE
@@ -13,30 +13,30 @@
  * @param human the human player to initialise
  **/
 enum input_result get_human_player(struct player* human)
-{	
+{
 
 	char userName[(NAMELEN + 2)];
 	int uLength;
-	
+
 
 	printf("%s\n", "What's yo name dawg?");
-	
+
 	fgets(userName, (NAMELEN + 2) , stdin);
-	
+
 	uLength = strlen(userName) - 1;
 	if (userName[uLength] == '\n'){
 		userName[uLength] = '\0';
 	}
 	printf("\e[1;1H\e[2J");
 	printf("Thanks, %s.\n", userName);
-	
+
 	/* construct player */
 	strcpy(human->name, userName);
 	human->thiscolor = C_RED;
 	human->counters = 0;
 	human->type = HUMAN;
-	
-	
+
+
 
     return SUCCESS;
 }
@@ -46,10 +46,10 @@ enum input_result get_human_player(struct player* human)
  **/
 enum input_result get_computer_player(struct player * computer)
 {
-    /* initialise all variables that are part of the struct to sensible 
+    /* initialise all variables that are part of the struct to sensible
      * values */
 	computer->thiscolor = C_WHITE;
-	computer->name = 'computer';
+	strcpy(computer->name, 'computer');
 	computer->counters = 0;
 	computer->type = COMPUTER;
 
@@ -60,20 +60,20 @@ enum input_result get_computer_player(struct player * computer)
  * In this requirement, you need to handle the taking of a turn - either
  * of a human or a computer player.
  *
- * For the human player, you will need to allow the user to enter the 
- * column they wish to place a token in. You will need to validate what 
+ * For the human player, you will need to allow the user to enter the
+ * column they wish to place a token in. You will need to validate what
  * the user enters, then place a token correctly in that column so that
- * it occupies the cell closest to the bottom of the board array for that 
+ * it occupies the cell closest to the bottom of the board array for that
  * column.
  *
- * For the computer player, you will need to randomly generate a column 
- * number to place a token in and if that column is full, generate a 
- * different column number until a column number with some free space has 
+ * For the computer player, you will need to randomly generate a column
+ * number to place a token in and if that column is full, generate a
+ * different column number until a column number with some free space has
  * been found.
  *
  * @param current the current player
  * @param board the game board that we will attempt to insert a token into
- * @return enum @ref input_result indicating the state of user input (in 
+ * @return enum @ref input_result indicating the state of user input (in
  * case this run involved user input
  **/
 enum input_result take_turn(struct player * current,
@@ -82,32 +82,35 @@ enum input_result take_turn(struct player * current,
 	if (current->type == HUMAN){
 		char input[3];
 		char *end;
+		int selection;
 		int iLength;
 		int count;
+		int col;
+
 		printf("%s\n", "It's your turn! \nPick a column from 1 to 7:");
-		
+
 		/* Get user selection & clear line*/
 		fgets(input, 3 , stdin);
-		
+
 		/* Format and fix newline chars */
 		iLength = strlen(input) - 1;
 		if (input[iLength] == '\n'){
 			input[iLength] = '\0';
 		}
-		
+
 		/* Convert read input string to int */
 		selection = (int) strtol(input, &end, 10);
-		
+
 		/* Log to console user selection and change board */
-		
+
 		printf("You selected: %d\n", selection);
-		
+
 		for (col = 0; col < BOARDHEIGHT; col++){
 			if (board[BOARDHEIGHT][selection] = C_EMPTY;) {
 				count == col;
 			}
 		}
-		
+
 		if (count) {
 			printf("%s\n", "This is a valid move");
 			board[count][selection] = current->thiscolor;
@@ -115,14 +118,14 @@ enum input_result take_turn(struct player * current,
 			printf("%s\n", "This isnt a valid move! Try again m8..");
 			/* use this entire thing in do while. */
 		}
-		
-		
+
+
 	} else if (current-type == COMPUTER){
 		/* generate random number.*/
 	}
-	
+
 	/*  call gamestate. */
-    
+
 	/*
      * Default return value - delete this comment and the return
      * value and replace it with appropriate logic to handle either
